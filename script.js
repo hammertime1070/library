f0ca6f57e36157acdae7759a5bd0a0f0b57b0ad6
hammertime1070 <mbkeith614@gmail.com>
2023-09-12 17:12:36 -0400

    Add Book object and addBookToLibrary function

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const myLibrary = [];
 
-function Book() {
-    // the constructor
+function Book(title, author, pageCount) {
+    this.title = title,
+    this.author = author,
+    this.pageCount = pageCount
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(Book) {
+    myLibrary.push(Book)
+}
+
+const testBook = Book('test', 'test author', '100')
+
+function displayLibrary() {
+    // Display the library using DOM
 }
